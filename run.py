f71e4b9ed820b38ab437371a93709082591d39a0
Matthias <tqzamf@gmail.com>
2019-08-07 20:02:54 +0200

    convert VDI to compressed qcow2 with backing file
    this produces fairly small files which can still be mounted using
    qemu-nbd.

--- run.py
+++ run.py
@@ -15,14 +15,35 @@ def getline(cmd, key, regex, verbose=True):
 			print line.rstrip()
 	raise Exception('"%s" not found in output of %s' % (key, cmd))
 
+def system(cmd):
+	subprocess.check_call(cmd, shell=True)
+
 def clonedisk(task):
 	uuid = getline('VBoxManage list vms -l', 'SATA (0, 0): ',
 		'\{([0-9a-f-]{36})\}', verbose=False)
 	print 'rootdisk', uuid
-	clone = getline('VBoxManage clonemedium disk %s temp-images/%s.vdi'
-		% (uuid, task), 'Clone medium created ','UUID: ([0-9a-f-]{36})')
+	vdi = 'temp-images/%s-full.vdi' % task
+	clone = getline('VBoxManage clonemedium disk %s %s' % (uuid, vdi),
+			'Clone medium created ','UUID: ([0-9a-f-]{36})')
 	print 'clone', clone
-	os.system('VBoxManage closemedium ' + clone)
+	system('VBoxManage closemedium %s' % clone)
+	# this convoluted method creates a compressed image as a diff of a base
+	# image called base.qcow2, which must exist. it's usually worth it though:
+	# this turns 9GB full-disk images into ~800MB diffs that can be mounted
+	# using qemu-nbd!
+	full = '%s-full.qcow2' % task
+	sparse = '%s-sparse.qcow2' % task
+	compressed = '%s.qcow2' % task
+	system('qemu-img convert -p -O qcow2 %s temp-images/%s' % (vdi, full))
+	os.remove(vdi)
+	system('qemu-img create -q -f qcow2 -F qcow2 -b %s temp-images/%s'
+			% (full, sparse))
+	system('cd temp-images && qemu-img rebase -p -f qcow2 -F qcow2 ' +
+			'-b base.qcow2 %s' % sparse)
+	os.remove('temp-images/' + full)
+	system('qemu-img convert -p -c -f qcow2 -O qcow2 -B base.qcow2 ' +
+			'temp-images/%s temp-images/%s' % (sparse, compressed))
+	os.remove('temp-images/' + sparse)
 
 def submit(path, timeout, route):
 	return getline('cuckoo submit --package exe --timeout %s -o route=%s "%s"'
